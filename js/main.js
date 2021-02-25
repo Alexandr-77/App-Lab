9da1c09ce52926b48a7b96fbd925f01c4531eab1
Sasha-Tes <salamon-77@mail.ru>
2021-02-25 23:35:35 +0200

    Доделан клик кнопки

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 function month() {
    document.getElementById("Monthly").style.display = "flex"
    document.getElementById("Annually").style.display = "none"
+   document.getElementById("active-extra").style.display = "flex"
+   document.getElementById("active-extra-2").style.display = "none"
 };
 
 
 function year() {
    document.getElementById("Annually").style.display = "flex"
    document.getElementById("Monthly").style.display = "none"
+   document.getElementById("active-extra-2").style.display = "flex"
+   document.getElementById("active-extra").style.display = "none"
 };
+
+
+
